928ded05841b985ad8486b0d0a7348a8afc8eb59
TamNhien <canhthien91@gmail.com>
2024-09-17 14:42:12 +0700

    1

---
+++ src/spc/edu/Bai11_TinhTienVonVaLai.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+import  java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
+public class Bai11_TinhTienVonVaLai {
+    public static void main(String[] args) {
+        System.out.println("Tinh tien von va lai");
+        Scanner sc = new Scanner(System.in);
+        double T;
+        System.out.print("Nhap so tien gui ban dau (trieu dong): ");
+        T = sc.nextDouble();
+        
+        for (int year = 1; year <= 15;  year++) {
+            double tien_von = T * Math.pow(1 + 0.08, year);
+            double tien_lai = T * 0.08 * year;
+            
+            System.out.printf("Nam: %d\tTien von: %.2f trieu dong\tTien lai: %.2f trieu dong%n", year, tien_von, tien_lai);
+        }
+        sc.close();
+    }
+}
---
+++ src/spc/edu/Bai16_ChuyenHeCoSo10.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+import java.util.Scanner;
+/**
+ *
+ * @author Administrator
+ */
+public class Bai16_ChuyenHeCoSo10 {
+    public static void doiCoSo(int n, int base){
+        if (n >= base) doiCoSo (n / base, base);
+        if (n % base > 9) System.out.printf("%c", n % base + 55);
+        else
+            System.out.print((n % base));
+    }
+    
+    public static int nhap(){
+        Scanner sc = new Scanner(System.in);
+        boolean check = false;
+        int n = 0;
+        while (!check){
+            System.out.print(" ");
+            try {
+                n = sc.nextInt();
+                check = true;
+            } catch(Exception e){
+                System.out.println("Ban phai nhap so! hay nhap lai...");
+                sc.nextLine();
+            }
+        }
+        return(n);
+    }
+    public static void main(String[] args) {
+        System.out.print("Nhap n: ");
+        int n = nhap();
+        System.out.print("Nhap vao co so can chuyen sang b: ");
+        int b = nhap();
+        System.out.printf("So %d chuyen sang co so %d thanh: ",n,b);
+        doiCoSo(n,b);
+    }
+}
