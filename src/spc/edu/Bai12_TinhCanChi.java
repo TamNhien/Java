fa68e2bf7b44435a2fcf4a7e3801522a01c9e041
TamNhien <canhthien91@gmail.com>
2024-10-01 22:07:34 +0700

    cc

--- src/spc/edu/Bai12_TinhCanChi.java
+++ src/spc/edu/Bai12_TinhCanChi.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package spc.edu;
 import  java.util.Scanner;
 /**
@@ -10,20 +6,19 @@ import  java.util.Scanner;
  */
 public class Bai12_TinhCanChi {
     public static void main(String[] args) {
-        System.out.println("Tinh can chi");
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhap nam can tinh can chi: ");
         int nam = sc.nextInt();
         String can = TinhCan(nam);
         String chi = TinhChi(nam);
-        System.out.println("Nam " + nam + " la: " + can + " " + chi);
+        //System.out.println("Nam " + nam + " la: " + can + " " + chi);
+        System.out.printf("Nam %d la: %s %s\n", nam, can, chi);
     }
         
     static String TinhCan(int nam) {
         String[] canArray = { "Canh", "Tan", "Nham", "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ky" };
         return canArray[nam % 10];
     }
-
     static String TinhChi(int nam) {
         String[] chiArray = { "Than", "Dau", "Tuat", "Hoi", "Ty", "Suu", "Dan", "Mao", "Thin", "Ty", "Ngo", "Mui" };
         return chiArray[nam % 12];
--- src/spc/edu/moi/TinhTienVonVaLai.java
+++ src/spc/edu/moi/TinhTienVonVaLai.java
@@ -2,25 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package spc.edu;
-import  java.util.Scanner;
+package spc.edu.moi;
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
  */
-public class Bai11_TinhTienVonVaLai {
+public class TinhTienVonVaLai {
     public static void main(String[] args) {
-        System.out.println("Tinh tien von va lai");
         Scanner sc = new Scanner(System.in);
         double T;
         System.out.print("Nhap so tien gui ban dau (trieu dong): ");
         T = sc.nextDouble();
         
-        for (int year = 1; year <= 15;  year++) {
+        for (int year = 1; year <= 15; year++){
             double tien_von = T * Math.pow(1 + 0.08, year);
             double tien_lai = T * 0.08 * year;
             
-            System.out.printf("Nam: %d\tTien von: %.2f trieu dong\tTien lai: %.2f trieu dong%n", year, tien_von, tien_lai);
+            System.out.printf("Nam: %d\tTien von: %.2f trieu dong\tTien lai: %.2f trieu dong%n",year, tien_von, tien_lai);
         }
     }
 }
