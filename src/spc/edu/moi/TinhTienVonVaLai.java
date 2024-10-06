610ed363ddefc0de9719cb4e2dd779b5d9efc44f
TamNhien <canhthien91@gmail.com>
2024-10-06 20:13:02 +0700

    cc

--- src/spc/edu/moi/TinhTienVonVaLai.java
+++ src/spc/edu/moi/TinhTienVonVaLai.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package spc.edu.moi;
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
 public class TinhTienVonVaLai {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
