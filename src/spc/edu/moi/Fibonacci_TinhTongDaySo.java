b2e341d23ccdd36902bd21a12d3dbab172eababf
TamNhien <canhthien91@gmail.com>
2024-10-06 19:20:43 +0700

    cc

--- src/spc/edu/moi/Fibonacci_TinhTongDaySo.java
+++ src/spc/edu/moi/Fibonacci_TinhTongDaySo.java
@@ -9,12 +9,13 @@ public class Fibonacci_TinhTongDaySo {
         
         int[] fibonacciArray = new int[n];
         if (n > 0) fibonacciArray[0] = 1;
-        if (n > 1) fibonacciArray[0] = 1;
+        if (n > 1) fibonacciArray[1] = 1;
         
         for (int i = 2; i < n; i++) {
            fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];  
         }
         int sum = 0;
+        System.out.print("Day so Fibonacci: ");
         for (int i = 1; i < n; i++) {
             System.out.print(fibonacciArray[i] + " ");
             sum += fibonacciArray[i];
