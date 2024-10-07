2ec1c442729712e2c39569c34921b8e8230e90a7
TamNhien <canhthien91@gmail.com>
2024-10-07 22:41:29 +0700

    cc

--- src/spc/edu/moi/LuyThua.java
+++ src/spc/edu/moi/LuyThua.java
@@ -1,10 +1,6 @@
 package spc.edu.moi;
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
 public class LuyThua {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
